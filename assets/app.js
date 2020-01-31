5a6b3f69e17fa2fe07083dfff5efb4f7ae12b3c8
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-31 12:51:15 -0800

    updated logic

--- assets/app.js
+++ assets/app.js
@@ -3,11 +3,41 @@
 //  categories range from 9 - 32
 
 let category = 9;
+let choices = [];
+let answers = [];
+let score = 0;
 
-fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`)
+
+// document.getElementById('beginGame').addEventListener('click', event => {
+//   category = document.getElementById('promptChoices').value;
+
+  fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`)
   .then(r => r.json())
-  .then(({ results }) =>{
-  console.log(results[0]);
-})
-.catch(e => console.error(e));
+  .then(({results}) =>{
+    results.forEach((elem, index) => {
+      let question = results[index].question;
+      let choices = (results[index].incorrect_answers)
+      let answer = results[index].correct_answer;
+      answers.push(answer);
+      choices.push(results[index].correct_answer);
+      shuffleArray(choices);
+      console.log(question, choices);
+      
+    });
+  })
+  .catch(e => console.error(e));
+  
+// })
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function renderQuestions(question, choices) {
+  
+}
+
+//to check for answers, if the one they select is answers.includes(answer selection) then score++
