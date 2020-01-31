c67d26682826ee26ba18cdc7dc0440d615c3a767
EvanErickson <evanserickson@gmail.com>
2020-01-31 12:43:18 -0800

    added js

--- assets/app.js
+++ assets/app.js
@@ -3,11 +3,32 @@
 //  categories range from 9 - 32
 
 let category = 9;
+let choices = []
 
-fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`)
+
+// document.getElementById('beginGame').addEventListener('click', (event) => {
+//   category = document.getElementById('promptChoices').value;
+  
+// })
+
+
+  fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`)
   .then(r => r.json())
   .then(({ results }) =>{
-  console.log(results[0]);
-})
-.catch(e => console.error(e));
+    results.forEach((elem, index) => {
+      let question = results[index].question;
+      let choices = (results[index].incorrect_answers);
+      choices.push(results[index].correct_answer);
+      results[index];
+      document.getElementById('title').innerHTML = `
+      ${question}
+      `
+      document.getElementById('body').innerHTML = `
+      ${choices}
+      `
 
+    });  
+  
+  })
+  .catch(e => console.error(e));
+  
